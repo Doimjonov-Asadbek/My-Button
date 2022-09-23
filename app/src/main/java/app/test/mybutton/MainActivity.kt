9b8b644d83d1780b2fbf708c50cf191e601b0830
UserNameAllambalo <doimjonovasadbek004@gmail.com>
2022-09-23 19:03:15 +0500

    Works

---
+++ app/src/main/java/app/test/mybutton/MainActivity.kt
@@ -0,0 +1,20 @@
+package app.test.mybutton
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+
+        button.setOnClickListener {
+                Toast.makeText(this, button.text, Toast.LENGTH_SHORT).show()
+            }
+
+    }
+}
